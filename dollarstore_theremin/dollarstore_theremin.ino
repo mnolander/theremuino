0c94a6fc91d89dbff901a33d5748fab0b778ba8b
Michael Nolander <113628428+mnolander@users.noreply.github.com>
2024-02-10 11:57:32 -0800

    Initial commit, set up photoresistor and ultrasonic

--- dollarstore_theremin/dollarstore_theremin.ino
+++ dollarstore_theremin/dollarstore_theremin.ino
@@ -4,68 +4,49 @@
 #define PHOTOSENSOR_PIN A0
 
 float volume = 0.0;
-float pitchControl = 0.0;
+float pitch = 0.0;
 
 void setup() {
   Serial.begin(9600);
 
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
   pinMode(BUZZER_PIN, OUTPUT);
-
   pinMode(ULTRASONIC_TRIGGER_PIN, OUTPUT);
   pinMode(ULTRASONIC_ECHO_PIN, INPUT);
-
-  // No need to initialize the photoresistor pin as it's an analog input
 }
 
 void loop() {
-  // Read button state
-  buttonState = digitalRead(BUTTON_PIN);
-
-  // Read ultrasonic sensor for volume control
   volume = getUltrasonicDistance(ULTRASONIC_TRIGGER_PIN, ULTRASONIC_ECHO_PIN);
-
-  // Read photoresistor for pitch/tone control
-  pitchControl = analogRead(PHOTOSENSOR_PIN);
+  pitch = analogRead(PHOTOSENSOR_PIN);
 
   // Map volume to the buzzer's range (0 to 255)
   int mappedVolume = map(static_cast<int>(volume), 0, 100, 0, 255);
-
   // Map pitch control to the buzzer's pitch range
-  int mappedPitch = map(static_cast<int>(pitchControl), 0, 1023, 500, 2000);
+  int mappedPitch = map(static_cast<int>(pitch), 0, 1023, 500, 2000);
 
+  // TEST OUTPUTS
   // Print pitch angle
   Serial.print("Ultrasonic:");
   Serial.println(volume);
-
   // Print pitch angle
   Serial.print("Pitch:");
-  Serial.println(pitchControl);
+  Serial.println(pitch);
 
-  // Play sound with the calculated pitch and volume if the button is not pressed
-  if (digitalRead(BUTTON_PIN) == HIGH) {
-    tone(BUZZER_PIN, mappedPitch, mappedVolume);
-  } else {
-    Serial.println("Muted!");
-    noTone(BUZZER_PIN);
-  }
+  // Play tone based on calculated volume+pitch
+  tone(BUZZER_PIN, mappedPitch, mappedVolume);
 
-  // Add a delay to prevent excessive readings
   delay(50);
 }
 
 float getUltrasonicDistance(int triggerPin, int echoPin) {
-  // Trigger ultrasonic sensor
   digitalWrite(triggerPin, LOW);
   delayMicroseconds(2);
   digitalWrite(triggerPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(triggerPin, LOW);
 
-  // Read the echo pulse duration
   long duration = pulseIn(echoPin, HIGH);
 
-  // Calculate distance in centimeters
+  // Calculate distance in cm
   float distance = duration * 0.034 / 2;
 
   // Limit the distance to a reasonable range (0 to 100 cm)
